6e874cee71f46e38c0b17564b9667187ad699ada
BTomi-prog <ban.tamas22i@egressy.info>
2026-05-16 10:42:29 +0200

    CarrtItem Done.

--- ShoppingCartApp/CartItem.cs
+++ ShoppingCartApp/CartItem.cs
@@ -9,19 +9,40 @@ namespace ShoppingCartApp
         // name nem lehet null/üres, unitPrice > 0, quantity >= 1
         public CartItem(string name, double unitPrice, int quantity)
         {
-            throw new NotImplementedException();
+            if (name == null || name == "")
+            {
+                throw new ArgumentException("Nem lett név megadva");
+            }
+            if (unitPrice <= 0 )
+            {
+                throw new ArgumentException("A unit price-nak 0-nál nagyobb számnak kell lennie");
+            }
+            if (quantity < 1)
+            {
+                throw new ArgumentException("A quantity 1 vagy 1-nél nagyobb számnak kell lennie");
+            }
+            Name = name;
+            UnitPrice = unitPrice;
+            Quantity = quantity;
         }
 
         // UnitPrice * Quantity
         public double GetLineTotal()
         {
-            throw new NotImplementedException();
+            return UnitPrice * Quantity;
         }
 
         // quantity >= 1, különben ArgumentException
         public void UpdateQuantity(int quantity)
         {
-            throw new NotImplementedException();
+            if (quantity < 1)
+            {
+                throw new ArgumentException("A quantity 1 vagy 1-nél nagyobb számnak kell lennie");
+            }
+            else
+            {
+                Quantity = quantity;
+            }
         }
     }
 }
